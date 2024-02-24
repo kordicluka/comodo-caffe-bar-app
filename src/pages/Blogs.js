7ec56ebabf4c280365850dc390f57bb97a65a8ff
Luka Korda <luka.kordic.zg@gmail.com>
2024-02-24 21:38:41 +0100

    ad

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -7,26 +7,26 @@ import "../styles/Blogs.scss";
 const Blogs = () => {
   const [blogs, setBlogs] = useState([
     {
-      title: "Najava kviza 25.2.2024. u Comodo Bar-u.",
+      title: "Najava kviza Bitange i Princeze 29.2.2024.",
       shortDescription: "Kliknite za prijavu na kviz!",
       content: "<p>This is the content for blog 1</p>",
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
       images: [
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708798355/Screenshot_2024-02-24_191216_urfwwc.png",
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708798122/Screenshot_2024-02-24_190751_tlwv1m.png",
       ],
       slug: "najava-kviza-25-2-2024",
     },
     {
-      title: "Održan kviz Bitangi i Princeza 18.2.2024. u Comodo Bar-u.",
+      title: "Održan kviz  18.2.2024. u Comodo Bar-u.",
       shortDescription: "Pogledajte kako je bilo na kvizu!",
       content: "<p>This is the content for blog 1</p>",
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
       images: [
-        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.3&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-D50PRX1K9h2LK95ARWvHPrLK-smAu8R6nkPqopYqdZ6M4-EoEnhwRBcnmOAuQ0q2QIQsJeyXqabFwkn2zFSJYJqxOg2xTjz2gI-A4tmut1PY4ljVsIFW3kwE&disp=emb&realattid=1894bffdebb6d1c2e478",
-        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.4&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-kqQfbc6Z74CQDNbxE3A6EpXG6CiwYHSk2C91ACPgwUpzy5uLw2t0O8XWLRIzilTrzFrSuXJ6zZc2kpKYHkQ9DxBinJYQr_IIKzJa6YFfkytknNwyLDU1KNlU&disp=emb&realattid=1894bffbb2265cd24477",
-        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.8&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ8W8PT7Ue2dg8alNGVbaQkH6pYUftwihVHU82FbYRJW6EJluIGNnVUs1DBypXJUwlMW4hc_7DPNTorWlnXeAQiw1iJqwNRO6_oflQfNeYIlDeGYDLiyJb0hgDk&disp=emb&realattid=1894bff7c3a277da8e55",
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_5_yvcfsy.jpg",
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_3_ak4juq.jpg",
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_4_cmtkt4.jpg",
       ],
       slug: "odrzan-kviz-bitangi-i-princeza-18-2-2024",
     },
@@ -37,7 +37,7 @@ const Blogs = () => {
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
       images: [
-        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.4&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-kqQfbc6Z74CQDNbxE3A6EpXG6CiwYHSk2C91ACPgwUpzy5uLw2t0O8XWLRIzilTrzFrSuXJ6zZc2kpKYHkQ9DxBinJYQr_IIKzJa6YFfkytknNwyLDU1KNlU&disp=emb&realattid=1894bffbb2265cd24477",
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_4_cmtkt4.jpg",
       ],
       slug: "inquizicija-kviz-15-2-2024",
     },
@@ -49,7 +49,7 @@ const Blogs = () => {
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
       images: [
-        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.8&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ8W8PT7Ue2dg8alNGVbaQkH6pYUftwihVHU82FbYRJW6EJluIGNnVUs1DBypXJUwlMW4hc_7DPNTorWlnXeAQiw1iJqwNRO6_oflQfNeYIlDeGYDLiyJb0hgDk&disp=emb&realattid=1894bff7c3a277da8e55",
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_1_ltmhrq.jpg",
       ],
       slug: "proslava-rodendana-2-11-2024",
     },
@@ -59,7 +59,9 @@ const Blogs = () => {
       content: "<p>This is the content for blog 2</p>",
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/4.png"],
+      images: [
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_2_xn3abu.jpg",
+      ],
       slug: "inquizicija-kviz-15-2-2024",
     },
     {
@@ -69,7 +71,9 @@ const Blogs = () => {
       content: "<p>This is the content for blog 3</p>",
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/1.png"],
+      images: [
+        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708806996/unnamed_abflwn.jpg",
+      ],
       slug: "proslava-rodendana-2-11-2024",
     },
   ]);
