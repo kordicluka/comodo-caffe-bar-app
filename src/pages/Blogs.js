935333d100251d1bb4f8a0f6f11c80dfb4788ca1
Luka Korda <luka.kordic.zg@gmail.com>
2024-02-24 21:31:56 +0100

    ad

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -24,9 +24,9 @@ const Blogs = () => {
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
       images: [
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708798122/Screenshot_2024-02-24_190751_tlwv1m.png",
-        "https://www.gracin.hr/wp-content/uploads/2022/12/4.png",
-        "https://www.gracin.hr/wp-content/uploads/2022/12/1.png",
+        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.3&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-D50PRX1K9h2LK95ARWvHPrLK-smAu8R6nkPqopYqdZ6M4-EoEnhwRBcnmOAuQ0q2QIQsJeyXqabFwkn2zFSJYJqxOg2xTjz2gI-A4tmut1PY4ljVsIFW3kwE&disp=emb&realattid=1894bffdebb6d1c2e478",
+        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.4&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-kqQfbc6Z74CQDNbxE3A6EpXG6CiwYHSk2C91ACPgwUpzy5uLw2t0O8XWLRIzilTrzFrSuXJ6zZc2kpKYHkQ9DxBinJYQr_IIKzJa6YFfkytknNwyLDU1KNlU&disp=emb&realattid=1894bffbb2265cd24477",
+        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.8&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ8W8PT7Ue2dg8alNGVbaQkH6pYUftwihVHU82FbYRJW6EJluIGNnVUs1DBypXJUwlMW4hc_7DPNTorWlnXeAQiw1iJqwNRO6_oflQfNeYIlDeGYDLiyJb0hgDk&disp=emb&realattid=1894bff7c3a277da8e55",
       ],
       slug: "odrzan-kviz-bitangi-i-princeza-18-2-2024",
     },
@@ -36,7 +36,9 @@ const Blogs = () => {
       content: "<p>This is the content for blog 2</p>",
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/4.png"],
+      images: [
+        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.4&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-kqQfbc6Z74CQDNbxE3A6EpXG6CiwYHSk2C91ACPgwUpzy5uLw2t0O8XWLRIzilTrzFrSuXJ6zZc2kpKYHkQ9DxBinJYQr_IIKzJa6YFfkytknNwyLDU1KNlU&disp=emb&realattid=1894bffbb2265cd24477",
+      ],
       slug: "inquizicija-kviz-15-2-2024",
     },
     {
@@ -46,64 +48,9 @@ const Blogs = () => {
       content: "<p>This is the content for blog 3</p>",
       dateStart: "15.02.2024.",
       dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/1.png"],
-      slug: "proslava-rodendana-2-11-2024",
-    },
-    {
-      title: "Inquizicija kviz 15.2.2024. u Comodo Bar-u.",
-      shortDescription: "Kliknite za prijavu na kviz!",
-      content: "<p>This is the content for blog 2</p>",
-      dateStart: "15.02.2024.",
-      dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/4.png"],
-      slug: "inquizicija-kviz-15-2-2024",
-    },
-    {
-      title: "Proslava rođendana u Comodo Bar-u.",
-      shortDescription:
-        "Pogledajte kako izgleada proslava rođendana u Comodo Bar-u!",
-      content: "<p>This is the content for blog 3</p>",
-      dateStart: "15.02.2024.",
-      dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/1.png"],
-      slug: "proslava-rodendana-2-11-2024",
-    },
-    {
-      title: "Inquizicija kviz 15.2.2024. u Comodo Bar-u.",
-      shortDescription: "Kliknite za prijavu na kviz!",
-      content: "<p>This is the content for blog 2</p>",
-      dateStart: "15.02.2024.",
-      dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/4.png"],
-      slug: "inquizicija-kviz-15-2-2024",
-    },
-    {
-      title: "Proslava rođendana u Comodo Bar-u.",
-      shortDescription:
-        "Pogledajte kako izgleada proslava rođendana u Comodo Bar-u!",
-      content: "<p>This is the content for blog 3</p>",
-      dateStart: "15.02.2024.",
-      dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/1.png"],
-      slug: "proslava-rodendana-2-11-2024",
-    },
-    {
-      title: "Inquizicija kviz 15.2.2024. u Comodo Bar-u.",
-      shortDescription: "Kliknite za prijavu na kviz!",
-      content: "<p>This is the content for blog 2</p>",
-      dateStart: "15.02.2024.",
-      dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/4.png"],
-      slug: "inquizicija-kviz-15-2-2024",
-    },
-    {
-      title: "Proslava rođendana u Comodo Bar-u.",
-      shortDescription:
-        "Pogledajte kako izgleada proslava rođendana u Comodo Bar-u!",
-      content: "<p>This is the content for blog 3</p>",
-      dateStart: "15.02.2024.",
-      dateEnd: "15.02.2024.",
-      images: ["https://www.gracin.hr/wp-content/uploads/2022/12/1.png"],
+      images: [
+        "https://mail.google.com/mail/u/0?ui=2&ik=f9d78bdd4b&attid=0.8&permmsgid=msg-f:1771251674760531862&th=1894c00381eacf96&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ8W8PT7Ue2dg8alNGVbaQkH6pYUftwihVHU82FbYRJW6EJluIGNnVUs1DBypXJUwlMW4hc_7DPNTorWlnXeAQiw1iJqwNRO6_oflQfNeYIlDeGYDLiyJb0hgDk&disp=emb&realattid=1894bff7c3a277da8e55",
+      ],
       slug: "proslava-rodendana-2-11-2024",
     },
     {
@@ -126,6 +73,16 @@ const Blogs = () => {
       slug: "proslava-rodendana-2-11-2024",
     },
   ]);
+
+  const [search, setSearch] = useState("");
+
+  const checkSearch = (title) => {
+    if (title.toLowerCase().includes(search.toLowerCase())) {
+      return true;
+    }
+
+    return false;
+  };
 
   return (
     <div className="page blogs">
@@ -151,7 +108,11 @@ const Blogs = () => {
           <h1>Događanja</h1>
         </div>
         <div className="search-bar">
-          <input type="text" placeholder="Pretraži događanja" />
+          <input
+            type="text"
+            placeholder="Pretraži događanja"
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -169,9 +130,11 @@ const Blogs = () => {
         </div>
       </div>
       <div className="blogs-container">
-        {blogs.map((blog, index) => (
-          <BlogCard blog={blog} index={index} />
-        ))}
+        {blogs.map((blog, index) => {
+          if (checkSearch(blog.title)) {
+            return <BlogCard blog={blog} index={index} />;
+          }
+        })}
       </div>
     </div>
   );
