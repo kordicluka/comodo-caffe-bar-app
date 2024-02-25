bf84c8cf12c801ff2d8afcdbafa4ce9c129847a4
Luka Korda <luka.kordic.zg@gmail.com>
2024-02-25 14:36:42 +0100

    asdasd

--- src/pages/AppLoading.js
+++ src/pages/AppLoading.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "../styles/AppLoading.scss";
 import logo from "../assets/logo.svg";
+import galerija from "../assets/images/gallery-1.jpg";
+
 const AppLoading = () => {
   const [loading, setLoading] = useState(true);
   // after 3 seconds push the page to the home page
@@ -14,7 +16,10 @@ const AppLoading = () => {
     window.location.href = "/menu";
   }
   return (
-    <div className="app-loading">
+    <div
+      className="app-loading"
+      style={{ backgroundImage: `url(${galerija})` }}
+    >
       <img src={logo} alt="logo" />
       <h1>Dobrodošli u caffe bar Comodo!</h1>
     </div>
