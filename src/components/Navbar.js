1334829837cd59086e431aae212376f9526e4700
Luka Korda <luka.kordic.zg@gmail.com>
2024-02-25 14:48:54 +0100

    asdasd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Navbar.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import blogIcon from "../assets/icons/blog.svg";
 import menuIcon from "../assets/icons/drinks.svg";
 import galleryIcon from "../assets/icons/gallery.svg";
@@ -9,22 +9,22 @@ import homeIcon from "../assets/icons/home.svg";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link className="navbar-link" to="/home">
+      <NavLink className="navbar-link" to="/home">
         <img src={homeIcon} alt="Home" />
         <span>Home</span>
-      </Link>
-      <Link className="navbar-link" to="/menu">
+      </NavLink>
+      <NavLink className="navbar-link" to="/menu">
         <img src={menuIcon} alt="Menu" />
         <span>Cjenik</span>
-      </Link>{" "}
-      <Link className="navbar-link" to="/blogs">
+      </NavLink>{" "}
+      <NavLink className="navbar-link" to="/blogs">
         <img src={blogIcon} alt="Blogs" />
         <span>Događanja</span>
-      </Link>
-      <Link className="navbar-link" to="/gallery">
+      </NavLink>
+      <NavLink className="navbar-link" to="/gallery">
         <img src={galleryIcon} alt="Gallery" />
         <span>Galerija</span>
-      </Link>
+      </NavLink>
     </div>
   );
 };
