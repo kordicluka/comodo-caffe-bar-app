986b2573cfe1d39fb594d439c327f9bb31b67add
Luka Korda <luka.kordic.zg@gmail.com>
2024-02-26 17:35:54 +0100

    asdasd

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -10,6 +10,8 @@ const Menu = () => {
       title: "Coca Cola",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Coca Cola je gazirano bezalkoholno piće koje se proizvodi od strane kompanije Coca-Cola Company. Coca Cola je najprodavanije bezalkoholno piće u svetu.",
       image:
         "https://assets.turbologo.com/blog/en/2019/08/19085106/coca-cola-logo-2007.jpg",
     },
@@ -18,6 +20,8 @@ const Menu = () => {
       title: "Coca Cola Zero",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Coca Cola Zero je bezalkoholno gazirano piće koje se proizvodi od strane kompanije Coca-Cola Company. Coca Cola Zero je bez šećera.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cola_zero_qs3okx.webp",
     },
@@ -26,6 +30,8 @@ const Menu = () => {
       title: "Cedevita",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Cedevita je instant napitak koji se koristi za pripremu osvežavajućeg napitka. Cedevita je proizvod kompanije Atlantic Grupa.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cedevita_lkl5ap.jpg",
     },
@@ -34,6 +40,8 @@ const Menu = () => {
       title: "Limonada",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Limonada je osvežavajuće gazirano bezalkoholno piće koje se pravi od limuna, vode i šećera.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/limunada_vlkuhv.jpg",
     },
@@ -42,6 +50,8 @@ const Menu = () => {
       title: "Naranča",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Naranča je osvežavajuće gazirano bezalkoholno piće koje se pravi od naranče, vode i šećera.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/naranca_ro3efd.png",
     },
@@ -50,127 +60,20 @@ const Menu = () => {
       title: "Pago",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Pago je osvežavajuće gazirano bezalkoholno piće koje se pravi od voća, vode i šećera.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/pago_logo_glow_umnptq.png",
     },
 
     {
-      id: 1,
-      title: "Coca Cola",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://assets.turbologo.com/blog/en/2019/08/19085106/coca-cola-logo-2007.jpg",
-    },
-    {
-      id: 2,
-      title: "Coca Cola Zero",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cola_zero_qs3okx.webp",
-    },
-    {
-      id: 3,
-      title: "Cedevita",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cedevita_lkl5ap.jpg",
-    },
-    {
-      id: 4,
-      title: "Limonada",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/limunada_vlkuhv.jpg",
-    },
-    {
-      id: 5,
-      title: "Naranča",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/naranca_ro3efd.png",
-    },
-    {
-      id: 6,
-      title: "Pago",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/pago_logo_glow_umnptq.png",
-    },
-    {
-      id: 1,
-      title: "Coca Cola",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://assets.turbologo.com/blog/en/2019/08/19085106/coca-cola-logo-2007.jpg",
-    },
-    {
-      id: 2,
-      title: "Coca Cola Zero",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cola_zero_qs3okx.webp",
-    },
-    {
-      id: 3,
-      title: "Cedevita",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cedevita_lkl5ap.jpg",
-    },
-    {
-      id: 4,
-      title: "Limonada",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/limunada_vlkuhv.jpg",
-    },
-    {
-      id: 5,
-      title: "Naranča",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/naranca_ro3efd.png",
-    },
-    {
-      id: 6,
-      title: "Pago",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/pago_logo_glow_umnptq.png",
-    },
-    {
-      id: 7,
-      title: "Schweppes Tonic",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/schweppes_tonic_ac11sz.jpg",
-    },
-    {
-      id: 8,
-      title: "Schweppes Bitter Lemon",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010562/schwpees_bitter_lemon_e29kv9.webp",
-    },
-    {
       id: 9,
       title: "Tangerine Juice",
       price: 2.5,
+
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Tangerine Juice is a refreshing non-alcoholic drink made from tangerine, water and sugar.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010562/tangerine_wijc2n.jpg",
     },
@@ -179,95 +82,19 @@ const Menu = () => {
       title: "Thomas Henry",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Thomas Henry is a refreshing non-alcoholic drink made from tangerine, water and sugar.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/thoma_henry_yb6mhv.webp",
     },
 
-    {
-      id: 1,
-      title: "Coca Cola",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://assets.turbologo.com/blog/en/2019/08/19085106/coca-cola-logo-2007.jpg",
-    },
-    {
-      id: 2,
-      title: "Coca Cola Zero",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cola_zero_qs3okx.webp",
-    },
-    {
-      id: 3,
-      title: "Cedevita",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010560/cedevita_lkl5ap.jpg",
-    },
-    {
-      id: 4,
-      title: "Limonada",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/limunada_vlkuhv.jpg",
-    },
-    {
-      id: 5,
-      title: "Naranča",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/naranca_ro3efd.png",
-    },
-    {
-      id: 6,
-      title: "Pago",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/pago_logo_glow_umnptq.png",
-    },
-    {
-      id: 7,
-      title: "Schweppes Tonic",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/schweppes_tonic_ac11sz.jpg",
-    },
-    {
-      id: 8,
-      title: "Schweppes Bitter Lemon",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010562/schwpees_bitter_lemon_e29kv9.webp",
-    },
-    {
-      id: 9,
-      title: "Tangerine Juice",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010562/tangerine_wijc2n.jpg",
-    },
-    {
-      id: 10,
-      title: "Thomas Henry",
-      price: 2.5,
-      category: ["Sva pića", "Sokovi"],
-      image:
-        "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/thoma_henry_yb6mhv.webp",
-    },
     {
       id: 11,
       title: "Pago",
       price: 2.5,
       category: ["Sva pića", "Sokovi"],
+      description:
+        "Pago is a refreshing non-alcoholic drink made from fruit, water and sugar.",
       image:
         "https://res.cloudinary.com/dvg2o5azu/image/upload/v1708010561/pago_logo_glow_umnptq.png",
     },
@@ -276,6 +103,8 @@ const Menu = () => {
       title: "Espresso",
       price: 1.6,
       category: ["Sva pića", "Topli napitci"],
+      description:
+        "Espresso is a concentrated form of coffee served in small, strong shots. It's known for its rich flavor and creamy texture, often forming a velvety foam on top known as crema. A perfect espresso is the foundation of many coffee drinks and is revered for its energy-boosting properties and deep, complex flavor profile.",
       image:
         "https://images.getrecipekit.com/20230414194230-espresso-cup-with-coffee-beans.jpg?aspect_ratio=16:9&quality=90&",
     },
